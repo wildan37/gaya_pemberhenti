8ce711c2bf1743f505ab1f8bb8930f9902088d04
Idan <edun_as@icloud.com>
2020-05-27 17:52:28 +0200

    actual Version of tddft and submit_job_td.py

--- sub_job_td.py
+++ sub_job_td.py
@@ -3,23 +3,50 @@
 import os
 import sys
 import re
+import subprocess
+import time
 import numpy as np
 
-ks = np.arange(10.0, 51.0, 20) 
-runs = np.arange(21, 31, 1) 
+ks = np.arange(170.0, 171.0, 20) 
+runs = np.arange(8, 9, 1) 
+Ts = np.arange(3500, 3501, 1000)
+Njobs_lim = 6
+Njobs_sub = 2
 
-def buildSubmit(filename, k, run):
+def buildSubmit(filename, k, run, T):
     inlines  = file('run_md.sh').readlines()
     outlines = []
     for line in inlines:
         if 'PARAMETERS' in line:
-            line = line.replace('PARAMETERS=(0 0)', 'PARAMETERS=(%.1f %d)' %(k, run))
+            line = line.replace('PARAMETERS=(0 0 0)', 'PARAMETERS=(%.1f %d %d)' %(k, run, T))
         outlines.append(line)
     file(filename, 'w').writelines(outlines)   
 
+i = 0
 for k in ks:
     for run in runs:
-        print 'sub_job_par_E%.1f_run%d' % (k, run)
-        filename_submit = 'submit_E%.1f_run%d.sh' % (k, run)
-        buildSubmit(filename_submit, k, run)
-        os.system('qsub ' + filename_submit)
+        for T in Ts:
+            print 'sub_job_par_E%.1f_run%d_T%dK' % (k, run, T)
+            filename_submit = 'submit_E%.1f_run%d_T%dK.sh' % (k, run, T)
+            buildSubmit(filename_submit, k, run, T)
+            os.system('/opt/torque/bin/qsub ' + filename_submit)
+            i+=1
+            wait = True
+            if i % Njobs_sub == 0:
+                while(wait):
+                    return_string = subprocess.Popen("/opt/torque/bin/qstat | grep 'Q' | grep 'abduss38' | wc -l",
+                                                    shell=True, stdout=subprocess.PIPE).communicate()
+                    waiting_jobs = int(np.fromstring(return_string[0], sep='\n')[0])
+                    return_string = subprocess.Popen("/opt/torque/bin/qstat | grep 'R' | grep 'abduss38' | wc -l",
+                                                    shell=True, stdout=subprocess.PIPE).communicate()
+                    running_jobs = int(np.fromstring(return_string[0], sep='\n')[0])
+
+                    # consider also qtop
+                    #subprocess.Popen("qtop | grep 'jobs running of'", shell=True, stdout=subprocess.PIPE).communicate()
+                    jobs_in_queue_total = running_jobs + waiting_jobs
+                    if jobs_in_queue_total < Njobs_lim:
+                        wait = False
+                    else:
+                        print '\n', "Waiting 64 s before continuing job submission ... since total jobs = %d" %(jobs_in_queue_total), '\n'
+                        time.sleep(64)
+
--- tddft_ehrenfest.py
+++ tddft_ehrenfest.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import os, time
-from ase.units import Hartree, Bohr, _amu, _me, AUT
+from ase.units import Hartree, Bohr, _amu, _me, AUT, kB
 from ase import Atoms
 from ase.parallel import paropen
 from gpaw.mpi import world
@@ -13,34 +13,43 @@ from gpaw.tddft.ehrenfest import EhrenfestVelocityVerlet
 import matplotlib.pyplot as plt
 from ase.visualize.plot import plot_atoms
 import sys
+import random as rd
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 4:
     sys.exit('Usage: database-name' % sys.argv[0])
 
 #Timestep and duration of simulation in attosecond
 en = float(sys.argv[1])
 run = int(sys.argv[2])
+T = int(sys.argv[3])
 #name = 'aluminum_h'
-name = 'boron_tetra_h'
+name = 'boron_beta_h_lsnap_T%dK'%(T)
 Ekin = en*1e3
 timestep = 1.0 * np.sqrt (10e3/Ekin)
-duration = 6e2
+
+if en > 51 : 
+    duration = 2e2
+    ndiv = int(np.ceil(0.01e3 / timestep))
+else : 
+    duration = 4e2
+    ndiv = int(np.ceil(0.02e3 / timestep))
+
 ekin_str = '_ek' + str(float(Ekin/1000)) + 'k_run' + str(run) 
 amu_to_aumass = _amu/_me
 strbody = name + ekin_str
-input_file = name + '.gpw'
+input_file = name + '_run' + str(run) + '.gpw'
 traj_file = strbody + '.traj'
 filename = strbody + '.txt'
 f = open (filename,"w")
-Natoms = 13
+Natoms = 106
 Nkord = 3
 p_bands = 2
-dom_dc = (2, 2, 8)
 print(en, run)
 #number of iterations and integer divisor to update states every 0.02 fs
-ndiv = int(np.ceil(0.02e3 / timestep))
+#ndiv = int(np.ceil(0.01e3 / timestep))
 niter = ndiv * int (np.ceil(duration / (ndiv * timestep) ) )
-
+#parallel = { 'band' : p_bands, 'domain' : None, 'sl_auto' : True}
+#parallel = { 'band' : p_bands, 'domain' : None, 'sl_default' : (4, 4, 64)}
 parallel = { 'band' : p_bands, 'domain' : None}
 tdcalc = TDDFT (input_file, solver = 'CSCG', 
                 propagator= 'EFSICN', parallel=parallel)
@@ -49,11 +58,33 @@ tdcalc = TDDFT (input_file, solver = 'CSCG',
 proj_idx = Natoms - 1
 v = np.zeros ((Natoms, 3))                            #set all velocity to zero
 Mproj = tdcalc.atoms.get_masses()[proj_idx]
+
+#Mat = []
+#vat = np.zeros([12, 3])
+#for i in range(0,12):
+#    Mat.append(tdcalc.atoms.get_masses()[i]*amu_to_aumass)
+
+#ekin projectile
 Ekin *= Mproj
 Ekin = Ekin / Hartree
-
 Mproj *= amu_to_aumass
 v[proj_idx, 0] = -np.sqrt ( ( 2 * Ekin) / Mproj ) * Bohr / AUT
+
+#ekin_atoms
+#Ekint = 1.5 * len(Mat) * kB * T
+##np.random.seed(run)
+#for i in range(0,12):
+#    for j in range(0,3):
+#        vat[i,j] = rd.uniform (0,1) / (3* Mat[i])**0.5
+#        v[i,j] = vat[i,j]
+#
+#world.broadcast(vat, 0)
+#ekint = 0.5 * np.dot(Mat, np.sum(vat**2, axis=1)) 
+#vat *= (Ekint/ekint)**0.5
+#for i in range(0,12):
+#    for j in range(0,3):
+#        v[i,j] = vat[i,j]
+
 tdcalc.atoms.set_velocities(v)
 
 evv = EhrenfestVelocityVerlet(tdcalc)
@@ -86,7 +117,7 @@ for i in range(niter):
             sp = 0
 
         f.write("%.6e,%.3lf,%.6e,%.6e,%.6e,%.6e,%.6e,%.6e,%.6e\n" 
-                %(T, xproj[m], ep, epot, rate, ekin, etot, epow[m], sp)) #all in  eV
+                %(T, xproj[m], ep, epot, v[proj_idx, 0], ekin, etot, epow[m], sp)) #all in  eV
 
         spc = SinglePointCalculator (spa, energy=epot, forces=F_av)
         spa.set_calculator(spc)
